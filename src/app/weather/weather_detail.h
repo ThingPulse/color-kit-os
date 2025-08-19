befb1695000b5d516d93a3f1a480ebe8e70c16c3
Daniel Eichhorn <dani.eichhorn@squix.ch>
2025-08-19 16:08:21 +0200

    Simplified code base

--- src/app/weather/weather_detail.h
+++
@@ -1,9 +0,0 @@
-#ifndef _WEATHER_IMAGE_H
-    #define _WEATHER_IMAGE_H
-
-    void weather_image_tile_setup( uint32_t tile_num );
-
-    void update_weather_detail(weather_config_t *weather_config, weather_forcast_t * hourly_forecast);
-
-#endif
-
